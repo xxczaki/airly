bdd3041834347e5ca26a0b5984b3dc55576e8dc1
Antoni Kepinski <xxczaki@pm.me>
2019-05-19 19:22:48 +0200

    Change test

--- test/test.ts
+++ test/test.ts
@@ -3,9 +3,9 @@ import Airly from '../source';
 
 const airly = new Airly('API_KEY');
 
-test('validation', async t => {
+test('invalid api key', async t => {
 	const error = await t.throwsAsync(async () => {
-		await airly.nearestInstallations(91.0, 181.0);
+		await airly.nearestInstallations(90.0, 180.0);
 	});
-	t.regex(error.message, /Expected number `91` to pass custom validation function/);
+	t.regex(error.message, /code/);
 });

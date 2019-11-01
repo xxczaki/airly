50e0f67786a3e260f3a2441977603ec0f8ccd6ce
Antoni Kepinski <xxczaki@pm.me>
2019-11-01 11:48:34 +0100

    Update examples

--- examples/id-info.js
+++ examples/id-info.js
@@ -6,7 +6,7 @@ const airly = new Airly('API_KEY');
 (async () => {
 	try {
 		// Get info about the installation number 245
-		const data = await airly.idInfo(245);
+		const data = await airly.installationInfo(245);
 
 		// Use the obtained data to fill this sentence
 		console.log(`This installation is located at ${data.address.street} Street in ${data.address.city}!`);

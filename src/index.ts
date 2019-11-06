c981f4ff61015434e3b14f345867ca95af417305
Antoni Kepinski <xxczaki@pm.me>
2019-11-06 20:47:47 +0100

    All functions now return Promise<any>, instead of Promise<string>

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ class Airly {
 	* @param {number} [maxResults=-1] Maximum number of installations to return; negative value means no limit
 	* @returns {Promise<object[]>} List of installations given by location point, ordered by distance to that point
 	*/
-	async nearestInstallations(lat: number, lng: number, maxDistanceKM = 3, maxResults = -1): Promise<string> {
+	async nearestInstallations(lat: number, lng: number, maxDistanceKM = 3, maxResults = -1): Promise<any> {
 		// @ts-ignore
 		const response = await fetch(`${this.baseUrl}/installations/nearest?lat=${lat}&lng=${lng}&maxDistanceKM=${maxDistanceKM}&maxResults=${maxResults}`, this.config).then((res: any) => res.json());
 		return response;
@@ -45,7 +45,7 @@ class Airly {
 	* @param {number} id Installation ID - Unique number of the installation
 	* @returns {Promise<object[]>} Information about specified installation
 	*/
-	async installationInfo(id: number): Promise<string> {
+	async installationInfo(id: number): Promise<any> {
 		// @ts-ignore
 		const response = await fetch(`${this.baseUrl}/installations/${id}`, this.config).then((res: any) => res.json());
 		return response;
@@ -55,7 +55,7 @@ class Airly {
 	* @param {number} id Installation ID - Unique number of the installation
 	* @returns {Promise<object[]>} Detailed measurements (current, historical and future) for an installation
 	*/
-	async installationMeasurements(id: number): Promise<string> {
+	async installationMeasurements(id: number): Promise<any> {
 		// @ts-ignore
 		const response = await fetch(`${this.baseUrl}/measurements/installation?installationId=${id}`, this.config).then((res: any) => res.json());
 		return response;
@@ -67,7 +67,7 @@ class Airly {
 	* @param {number} [maxDistanceKM=3] All the returned installations must be located within this limit from the given point (in km); negative value means no limit
 	* @returns {Promise<object[]>} Detailed measurements (current, historical and future) from the installation which is closest to a given point
 	*/
-	async nearestMeasurements(lat: number, lng: number, maxDistanceKM = 3): Promise<string> {
+	async nearestMeasurements(lat: number, lng: number, maxDistanceKM = 3): Promise<any> {
 		// @ts-ignore
 		const response = await fetch(`${this.baseUrl}/measurements/nearest?lat=${lat}&lng=${lng}&maxDistanceKM=${maxDistanceKM}`, this.config).then((res: any) => res.json());
 		return response;
@@ -78,7 +78,7 @@ class Airly {
 	* @param {number} lng Longitude - Geographical coordinate
 	* @returns {Promise<object[]>} Detailed measurements (current, historical and future) for a map point, which can be interpolated from nearby installations
 	*/
-	async pointMeasurements(lat: number, lng: number): Promise<string> {
+	async pointMeasurements(lat: number, lng: number): Promise<any> {
 		// @ts-ignore
 		const response = await fetch(`${this.baseUrl}/measurements/point?lat=${lat}&lng=${lng}`, this.config).then((res: any) => res.json());
 		return response;
@@ -87,7 +87,7 @@ class Airly {
 	/**
 	* @returns {Promise<object[]>} List of IndexTypes supported by the platform
 	*/
-	async metaIndexes(): Promise<string> {
+	async metaIndexes(): Promise<any> {
 		// @ts-ignore
 		const response = await fetch(`${this.baseUrl}/meta/indexes`, this.config).then((res: any) => res.json());
 		return response;
@@ -96,7 +96,7 @@ class Airly {
 	/**
 	* @returns {Promise<object[]>} List of MeasurementTypes supported by the platform
 	*/
-	async metaMeasurements(): Promise<string> {
+	async metaMeasurements(): Promise<any> {
 		// @ts-ignore
 		const response = await fetch(`${this.baseUrl}/meta/measurements`, this.config).then((res: any) => res.json());
 		return response;
